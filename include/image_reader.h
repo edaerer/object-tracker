2d61eb90e41bf08bb22afaa3f6eb4c0f9a2f3919
Hakan Kargın <hakankargin03@gmail.com>
2025-08-22 20:59:39 +0300

    Reformat codes and remove unnecessary mp4 implementation

--- include/image_reader.h
+++ include/image_reader.h
@@ -1,7 +1,6 @@
 #ifndef IMAGE_READER_H
 #define IMAGE_READER_H
 
-
 #include <stdlib.h>
 
 typedef struct {
@@ -20,6 +19,5 @@ void init_reader(const char *path);
 void close_reader(const char *path);
 
 imgdat_s load_data();
-
 
 #endif // IMAGE_READER_H
