f52943e818bbd3b76b1a607613246efb33a70a1a
Hakan Kargın <hakankargin03@gmail.com>
2025-08-23 11:08:15 +0300

    Reformat webcam_reader

--- src/webcam_reader.c
+++ src/webcam_reader.c
@@ -1,3 +1,4 @@
+#define WEBCAM_READER
 #ifdef WEBCAM_READER
 
 #include <fcntl.h>
@@ -12,47 +13,53 @@
 #define HEIGHT  480
 
 static int fd;
+static int buf_len;
+static int req_type;
 static void *ptr;
-static struct v4l2_format fmt = {0};
-static struct v4l2_buffer buf = {0};
-static struct v4l2_buffer qbuf = {0};
-static struct v4l2_requestbuffers req = {0};
 
 void init_reader(const char *path) {
     fd = open(path, O_RDWR);
 
-    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    fmt.fmt.pix.width = WIDTH;
-    fmt.fmt.pix.height = HEIGHT;
-    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
-    fmt.fmt.pix.field = V4L2_FIELD_NONE;
+    struct v4l2_format fmt = {
+        .type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
+        .fmt.pix.width = WIDTH,
+        .fmt.pix.height = HEIGHT,
+        .fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV,
+        .fmt.pix.field = V4L2_FIELD_NONE,
+    };
+    struct v4l2_requestbuffers req = {
+        .count = 1,
+        .type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
+        .memory = V4L2_MEMORY_MMAP,
+    };
+    struct v4l2_buffer buf = {
+        .index = 0,
+        .type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
+        .memory = V4L2_MEMORY_MMAP,
+    };
+    struct v4l2_buffer qbuf = {
+        .index = 0,
+        .type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
+        .memory = V4L2_MEMORY_MMAP,
+    };
+
     ioctl(fd, VIDIOC_S_FMT, &fmt);
-
-    req.count = 1;
-    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    req.memory = V4L2_MEMORY_MMAP;
     ioctl(fd, VIDIOC_REQBUFS, &req);
-
-    buf.index = 0;
-    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buf.memory = V4L2_MEMORY_MMAP;
     ioctl(fd, VIDIOC_QUERYBUF, &buf);
 
+    buf_len = buf.length;
+    req_type = req.type;
     ptr = mmap(NULL, buf.length, PROT_READ | PROT_WRITE,
                MAP_SHARED, fd, buf.m.offset);
-
-    qbuf.index = 0;
-    qbuf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    qbuf.memory = V4L2_MEMORY_MMAP;
-    ioctl(fd, VIDIOC_QBUF, &qbuf);
-
     enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+    ioctl(fd, VIDIOC_QBUF, &qbuf);
     ioctl(fd, VIDIOC_STREAMON, &type);
 }
 
 void close_reader(const char *path) {
-    ioctl(fd, VIDIOC_STREAMOFF, &req.type);
-    munmap(ptr, buf.length);
+    ioctl(fd, VIDIOC_STREAMOFF, &req_type);
+    munmap(ptr, buf_len);
     close(fd);
 }
 
