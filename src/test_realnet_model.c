a46581f43321f75cb99b1f028972a28539162202
Hakan Kargın <hakankargin03@gmail.com>
2025-08-25 14:34:09 +0300

    .

--- src/test_realnet_model.c
+++ src/test_realnet_model.c
@@ -5,7 +5,7 @@
 #include <sod.h>
 
 int main(void) {
-    const char *zFile = "../images/test4.jpg";
+    const char *zFile = "../images/test.png";
 
     sod_realnet *pNet;
     int i, rc;
@@ -86,10 +86,11 @@ int main(void) {
                aBoxes[i].score, aBoxes[i].zName ? aBoxes[i].zName : "bilinmeyen");
 
         /* Eşik değeri 0.0 - tüm tespitleri kabul et */
-        if (aBoxes[i].score >= 0.0) {
+        if (aBoxes[i].score >= 7.0) {
             detected_cups++;
             /* Draw a blue box around detected object */
             sod_image_draw_bbox_width(color, aBoxes[i], 3, 0., 150., 255.); // Mavi renk
+            //break;
         }
     }
 
